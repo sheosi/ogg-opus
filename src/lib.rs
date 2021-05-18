35d33ed212448cf9936c925a6f291a1e1a31292c
sheosi <sertorbe@gmail.com>
2021-05-18 16:22:46 +0200

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -37,8 +37,54 @@ pub enum Error {
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    use std::fs::{File};
+    use std::io::{Cursor};
+
+        fn read_file_i16(path: &str) -> Vec<i16> {
+            let mut f = File::open(path).expect("no file found");
+            let (_, b) = wav::read(&mut f).unwrap();
+            b.try_into_sixteen().unwrap()
+        }
+
+        #[test]
+        fn dec_enc_empty() {
+            let audio = Vec::new();
+            let (opus,enc_fin_range) = crate::encode::<16000>(&audio).unwrap();
+            let (audio2, _,dec_fin_range) = crate::decode::<_,16000>(Cursor::new(opus)).unwrap();
+            assert_eq!(audio.len(), audio2.len()); // Should be the same, empty
+            assert_eq!(enc_fin_range, dec_fin_range);
+        }
+
+        #[test]
+        fn dec_enc_recording_big() {
+            let audio = read_file_i16("test_assets/big.wav");
+            let (opus,enc_fin_range) = crate::encode::<16000>(&audio).unwrap();
+            let (a2,_,dec_fin_range) = crate::decode::<_,16000>(Cursor::new(opus)).unwrap();
+            assert_eq!(dec_fin_range, enc_fin_range);
+            assert_eq!(audio.len(), a2.len());
+        }
+
+        #[test]
+        fn dec_enc_recording_small() {
+            let audio = read_file_i16("test_assets/small.wav");
+            let (opus, enc_fin_range) = crate::encode::<16000>(&audio).unwrap();
+            let (a2, _, dec_fin_range) = crate::decode::<_, 16000>(Cursor::new(opus)).unwrap();
+            assert_eq!(dec_fin_range, enc_fin_range);
+            assert_eq!(audio.len(), a2.len());
+        }
+
+        #[test]
+        // Record, encode, decode , encode and decode again, finally compare the
+        // first and second decodes, to make sure nothing is lost (can't compare
+        // raw audio as vorbis is lossy)
+        fn dec_enc_recording_whole() {
+            let audio = read_file_i16("test_assets/small.wav");
+            let (opus, enc_fr1) = crate::encode::<16000>(&audio).unwrap();
+            let (audio2, _, dec_fr1) = crate::decode::<_, 16000>(Cursor::new(opus)).unwrap();
+            let (opus2, enc_fr2) = crate::encode::<16000>(&audio2).unwrap();
+            let (audio3, _, dec_fr2) = crate::decode::<_, 16000>(Cursor::new(opus2)).unwrap();
+            assert_eq!(audio2.len(), audio3.len());
+            assert_eq!(enc_fr1, dec_fr1);
+            assert_eq!(enc_fr2, dec_fr2);
+        }
 }
